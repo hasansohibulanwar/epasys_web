b4fe4ab909aa23e068673ae44f2a57eec96feec1
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2022-10-31 22:00:58 +0700

    main commit

--- main/login.php
+++ main/login.php
@@ -11,7 +11,9 @@
 <?php 
     if(isset($_GET['pesan'])){
         if($_GET['pesan']=="gagal"){
-            echo "<div class='alert'>Username dan Password tidak sesuai !</div>";
+            // echo "<div class='alert'>Username dan Password tidak sesuai !</div>";
+			$alert ="<script>alert('Username dan Password tidak sesuai') </script>";
+			echo $alert;
         }
     }
     ?>
