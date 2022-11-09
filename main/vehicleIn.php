b6dcbd091791226349b53b29464a08e4db9a9a02
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2022-11-10 01:22:00 +0700

    main commit

--- main/vehicleIn.php
+++ main/vehicleIn.php
@@ -16,9 +16,7 @@
 	?>
 
     <section id="content">
-	<?php
-		include 'includes/navigation.php'
-	?>
+	
 
         <main>
 			<div class="head-title">
@@ -40,122 +38,7 @@
 				</a> -->
 			</div>
 
-			<ul class="box-info">
-				<li>
-				<i class='bx bxs-parking'></i>
-					<span class="text">
-						<h3>100</h3>
-						<p>Kendaraan Parkir</p>
-					</span>
-				</li>
-				<li>
-					<!-- <i class='bx bxs-group' ></i> -->
-					<i class="bx"><ion-icon name="enter-outline"></ion-icon></i>
-					<span class="text">
-						<h3>80</h3>
-						<p>Kendaraan Masuk</p>
-					</span>
-				</li>
-				<li>
-					<!-- <i class='bx bxs-dollar-circle' ></i> -->
-					<i class='bx'><ion-icon name="exit-outline"></ion-icon></i>
-					<span class="text">
-						<h3>20</h3>
-						<p>Kendaraan Keluar</p>
-					</span>
-				</li>
-			</ul>
-
-
-			<div class="table-data">
-				<div class="order">
-					<div class="head">
-						<h3>Recent Orders</h3>
-						<i class='bx bx-search' ></i>
-						<i class='bx bx-filter' ></i>
-					</div>
-					<table>
-						<thead>
-							<tr>
-								<th>User</th>
-								<th>Date Order</th>
-								<th>Status</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>
-									<img src="img/people.png">
-									<p>John Doe</p>
-								</td>
-								<td>01-10-2021</td>
-								<td><span class="status completed">Completed</span></td>
-							</tr>
-							<tr>
-								<td>
-									<img src="img/people.png">
-									<p>John Doe</p>
-								</td>
-								<td>01-10-2021</td>
-								<td><span class="status pending">Pending</span></td>
-							</tr>
-							<tr>
-								<td>
-									<img src="img/people.png">
-									<p>John Doe</p>
-								</td>
-								<td>01-10-2021</td>
-								<td><span class="status process">Process</span></td>
-							</tr>
-							<tr>
-								<td>
-									<img src="img/people.png">
-									<p>John Doe</p>
-								</td>
-								<td>01-10-2021</td>
-								<td><span class="status pending">Pending</span></td>
-							</tr>
-							<tr>
-								<td>
-									<img src="img/people.png">
-									<p>John Doe</p>
-								</td>
-								<td>01-10-2021</td>
-								<td><span class="status completed">Completed</span></td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				<div class="todo">
-					<div class="head">
-						<h3>Todos</h3>
-						<i class='bx bx-plus' ></i>
-						<i class='bx bx-filter' ></i>
-					</div>
-					<ul class="todo-list">
-						<li class="completed">
-							<p>Todo List</p>
-							<i class='bx bx-dots-vertical-rounded' ></i>
-						</li>
-						<li class="completed">
-							<p>Todo List</p>
-							<i class='bx bx-dots-vertical-rounded' ></i>
-						</li>
-						<li class="not-completed">
-							<p>Todo List</p>
-							<i class='bx bx-dots-vertical-rounded' ></i>
-						</li>
-						<li class="completed">
-							<p>Todo List</p>
-							<i class='bx bx-dots-vertical-rounded' ></i>
-						</li>
-						<li class="not-completed">
-							<p>Todo List</p>
-							<i class='bx bx-dots-vertical-rounded' ></i>
-						</li>
-					</ul>
-				</div>
-			</div>
+			
 		</main>
 		<!-- MAIN -->
     </section>
