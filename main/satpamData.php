ece8d6e28d6f99d8a3a280a608436689977abea7
rsydfhmy03 <fahmyrosyadi29@gmail.com>
2022-12-08 10:10:20 +0700

    menambahkan Read data

--- main/satpamData.php
+++ main/satpamData.php
@@ -46,6 +46,58 @@
 				</a> -->
 			</div>
 
+			<div class="table-data">
+			<div class="order">
+				<div class="head">
+					<h3>Data Satpam</h3>
+					<i class='bx bx-search' ></i>
+					<i class='bx bx-filter' ></i>
+			</div>
+		<table id="example" class="display" style="width:100%">
+        <thead>
+            <tr>
+			<th>No</th>
+			<th>Nama</th>
+			<th>Email</th>
+			<th>Alamat</th>
+			<th>Gambar</th>
+			<th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+			<?php $i =1;?>
+            <?php 
+            include 'includes/fetch_functions.php';
+			$data = query("SELECT * FROM employees WHERE role='SATPAM'");
+            
+			foreach($data as $row) :
+				// while ($row = mysqli_fetch_assoc($result)) :
+            ?>
+            <tr>
+                <td><?= $i ?></td>
+                <td><?= $row["nama"]?></td>
+                <td><?= $row["email"]?></td>
+				<td><?= $row["alamat"]?></td>
+				<td><img src="<?= $row["avatar"]?>" width="50px" height="50px"></td>
+                <td>
+                <a href="">Detail</a> |
+                <a href="">Hapus</a>
+            
+                </td>
+            </tr>
+			<?php $i++; ?>
+            <?php 
+			// endforeach; 
+				endforeach;
+			?>
+        </tbody>
+        <tfoot>
+            <tr>
+               
+            </tr>
+        </tfoot>
+    	</table>
+	</div>
 			
 		</main>
 		<!-- MAIN -->
--- main/vehicleData.php
+++ main/vehicleData.php
@@ -46,6 +46,60 @@
 				</a> -->
 			</div>
 
+		<div class="table-data">
+			<div class="order">
+				<div class="head">
+					<h3>Recent Activity</h3>
+					<i class='bx bx-search' ></i>
+					<i class='bx bx-filter' ></i>
+			</div>
+		<table id="example" class="display" style="width:100%">
+        <thead>
+            <tr>
+			<th>No</th>
+			<th>Merek</th>
+			<th>No Polisi</th>
+			<th>Foto_stnk</th>
+			<th>Foto_kendaraan</th>
+			<th>Aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+			<?php $i =1;?>
+            <?php 
+            include 'includes/fetch_functions.php';
+			$data = query("SELECT * FROM vechiles");
+            
+			foreach($data as $row) :
+				// while ($row = mysqli_fetch_assoc($result)) :
+            ?>
+            <tr>
+                <td><?= $i ?></td>
+                <td><?= $row["merek"]?></td>
+                <td><?= $row["no_polisi"]?></td>
+				<td><img src="<?= $row["foto_stnk"]?>" width="50px" height="50px"></td>
+                <td>
+					<img src="<?= $row["foto_kendaraan"]?>" width="50px" height="50px">
+				</td>
+                <td>
+                <a href="">Detail</a> |
+                <a href="">Hapus</a>
+            
+                </td>
+            </tr>
+			<?php $i++; ?>
+            <?php 
+			// endforeach; 
+				endforeach;
+			?>
+        </tbody>
+        <tfoot>
+            <tr>
+               
+            </tr>
+        </tfoot>
+    	</table>
+	</div>
 			
 			
 		</main>
