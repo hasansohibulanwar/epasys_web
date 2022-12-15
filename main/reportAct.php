eb895d725c43816dd2b95baf6cb18f880faf3afc
hasan sohib <aresuma307@gmail.com>
2022-12-15 10:11:56 +0700

    kendaraan masuk

--- main/reportAct.php
+++ main/reportAct.php
@@ -37,18 +37,128 @@
 						<li><i class='bx bx-chevron-right' ></i></li>
 						<li>
 							<a class="active" href="#">Laporan Aktivitas</a>
-						</li>
+						</li>				
 					</ul>
 				</div>
+
+				
+		
+
 				<a href="#" class="btn-download">
 					<i class='bx bxs-cloud-download' ></i>
-					<span class="text">Download PDF</span>
+					<span class="text">Ekspor Laporan Ke PDF</span>
 				</a>
-			</div>
+				
 
-			
+
+				
+
 		</main>
 		<!-- MAIN -->
+		<?php
+			 // Mengenalkan variabel teks
+			$SqlPeriode  = "";
+			$awalTgl 	 = "";
+			$akhirTgl	 = "";
+			$tglAwal 	 = "";
+			$tglAkhir 	 = "";
+
+			if(isset($_POST['btnTampil'])){
+				$tglAwal = isset($_POST['txtTglAwal']) ? $_POST['txttglAwal'] : "01-".date('m-y');
+				$tglAkhir = isset($_POST['txtTglAkhir']) ? $_POST['txttglAkhir'] : date('d-m-y');
+				$SqlPeriode = " where A.tglmasuk BETWEEN ' " .$tglAwal."' AND ' ".$tglAkhir."'";
+
+			}
+			else {
+				$tglAwal = "01-".date('m-y');
+				$tglAkhir = date('d-m-y');
+				$SqlPeriode = "where A.tglpesanan BETWEEN ' " .$awalTgl."' AND '".$akhirTgl."'";
+
+			}
+
+
+ ?>
+				<h4> Periode Tanggal <b><?php echo ($tglAwal); ?> </b> s/d <b><?php echo ($tglAkhir); ?></b></h4>
+				<div class="card shadow " > 
+				
+				</div>
+				</div class="card">
+				<div class="card-body" > 
+				<form action = "<?php $_SERVER['PHP_SELF'] ;  ?>" method="post" name ="form10" target ="_self">
+
+				<div class="row">
+				<div class="col-lg-3">
+					<input name="txtTglAwal" type="date" class="form-control" value="<?php echo $awalTgl; ?>" size="10" />
+				</div>
+				<div class="col-lg-4">
+				 <input name="txtTglAkhir" type="date" class="form-control" value="<?php echo $akhirTgl; ?>" size="10" />
+				</div>
+				<div class="col-lg-4">
+				 <input name="btnTampil" class="btn btn-success " type="submit"  value="Tampilkan" />
+				</div>
+				</div>
+
+
+
+				<div class="table-resposive table mt-2" id="dataTable" role="grid" aria-describedby="dataTable">
+				   <table class="table dataTable my-0" id="dataTable1"> 
+					<thead>
+						<tr>
+						<th>No</th>
+						<th>Plat Nomor</th>
+						<th>Merk Kendaraan</th>
+						<th>Tanggal Masuk</th>
+						<th>Tanggal Keluar</th>
+						</tr>
+
+					</thead>
+					<tbody>
+						
+					<?php
+					$Sql = "SELECT A.*, B.nama_supplier FROM baranngmasuk A, supplier B $SqlPeriode AND A.idsupplier=B.idsupplier ad A.tottalmasuk !='0'";
+					//$myQry = mysqli_query($mysqli, $Sql) or die ("Query salah : ".mysqli_error());
+					$nomor =0;
+					while ($myData = mysqli_fetch_array($myQry)) {
+						$Kode = $myData ['idmasuk'];
+					$nomor++;
+					?>
+					<tr>
+						<td><?php echo $nomor;?></td>
+						<td><?php echo $myData['idmasuk'];?></td>
+						<td><?php echo $myData['tglmasuk'];?></td>
+						<td><?php echo $myData['namasupplier'];?></td>
+						<td><?php echo $myData['totalmasuk'];?></td>
+						<td>
+							<?php
+							$Sqldetail ="SELECT A.*, B.nama_barang FROM detailmasuk A, barang B where A.idbarang=B.idbarang and A.idmasuk ='$Kode'";
+							//$myQrydetail  = mysqli_query($mysqli, $Sqldetail) or die ("Query salah :  " .mysqli_error());
+							$nomor = 1;
+							while ($myDatadetail = mysqli_fetch_array($myQrydetail)) {
+								$Kodedetail = $myDatadetail['idmasuk'];
+								echo $myDatadetail['nama_barang']."(".$myDatadetail['qtymasuk'].")"."<br>";
+							}
+						  ?>
+						</td>
+
+						<td><?php echo $myData['nomor_nota ']; ?></td>
+						</td>
+					</tr>
+					<?php 
+					} $nomor++;
+					?>
+					</tbody>
+
+				
+					
+			    </table>
+
+		</div>
+
+		<div class="row">
+			<div class="col-lg-3"></div>
+			<a href="modul/mod_cetak/cetak.php?awal=<?php echo $tglAwal; ?>&&akhir =<?php echo $tglAkhir; ?>" target="_blank" alt="Edit Data" class="btnbtn btn-primary">Cetak Laporan </a>
+		</div>
+
 	</section>
 	<!-- NAVBAR -->
 
--- main/vehicleOut.php
+++ main/vehicleOut.php
@@ -36,7 +36,7 @@
 						</li>
 						<li><i class='bx bx-chevron-right' ></i></li>
 						<li>
-							<a class="active" href="#">Kendaraan Keluar</a>
+							<a class="active" href="#">Kendaraan Masuk</a>
 						</li>
 					</ul>
 				</div>
@@ -45,7 +45,58 @@
 					<span class="text">Download PDF</span>
 				</a> -->
 			</div>
-
+			
+			<div class="table-data">
+			<div class="order">
+				<div class="head">
+					<h3>Recent Activity</h3>
+					<i class='bx bx-search' ></i>
+					<i class='bx bx-filter' ></i>
+			</div>
+		<table id="example" class="display" style="width:100%">
+        <thead>
+            <tr>
+			<th>No</th>
+			<th>Merek</th>
+			<th>Users</th>
+			<th>Alamat</th>
+            </tr>
+        </thead>
+        <tbody>
+			<?php $i =1;?>
+            <?php 
+            include 'includes/fetch_functions.php';
+			$data = query("SELECT * FROM parkings WHERE status='IN'");
+            
+			foreach($data as $row) :
+				// while ($row = mysqli_fetch_assoc($result)) :
+            ?>
+            <tr>
+                <td><?= $i ?></td>
+                <td><?= $row["nama"]?></td>
+                <td><?= $row["merek"]?></td>
+				<td><?= $row["nama"]?></td>
+				<td><?= $row["alamat"]?></td>
+				<td><img src="<?= $row["avatar"]?>" width="50px" height="50px"></td>
+                <td>
+                <a href="">Detail</a> |
+                <a href="">Hapus</a>
+            
+                </td>
+            </tr>
+			<?php $i++; ?>
+            <?php 
+			// endforeach; 
+				endforeach;
+			?>
+        </tbody>
+        <tfoot>
+            <tr>
+               
+            </tr>
+        </tfoot>
+    	</table>
+	</div>
 			
 		</main>
 		<!-- MAIN -->
